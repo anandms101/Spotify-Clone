2326cbb593a0463feae06b78c22629a39cc131ff
Anand Mohan Singh <anandmohan.singh101@gmail.com>
2020-07-29 09:25:37 +0530

    signup

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,10 +1,28 @@
 <?php
 	class Account {
 
+		private $con;
 		private $errorArray;
 
-		public function __construct() {
+		public function __construct($con) {
+			$this->con = $con;
 			$this->errorArray = array();
+		}
+
+		public function login($un, $pw) {
+
+			$pw = md5($pw);
+
+			$query = mysqli_query($this->con, "SELECT * FROM users WHERE username='$un' AND password='$pw'");
+
+			if(mysqli_num_rows($query) == 1) {
+				return true;
+			}
+			else {
+				array_push($this->errorArray, Constants::$loginFailed);
+				return false;
+			}
+
 		}
 
 		public function register($un, $fn, $ln, $em, $em2, $pw, $pw2) {
@@ -16,7 +34,7 @@
 
 			if(empty($this->errorArray) == true) {
 				//Insert into db
-				return true;
+				return $this->insertUserDetails($un, $fn, $ln, $em, $pw);
 			}
 			else {
 				return false;
@@ -31,6 +49,16 @@
 			return "<span class='errorMessage'>$error</span>";
 		}
 
+		private function insertUserDetails($un, $fn, $ln, $em, $pw) {
+			$encryptedPw = md5($pw);
+			$profilePic = "assets/images/profile-pics/head_emerald.png";
+			$date = date("Y-m-d");
+
+			$result = mysqli_query($this->con, "INSERT INTO users VALUES ('', '$un', '$fn', '$ln', '$em', '$encryptedPw', '$date', '$profilePic')");
+
+			return $result;
+		}
+
 		private function validateUsername($un) {
 
 			if(strlen($un) > 25 || strlen($un) < 5) {
@@ -38,7 +66,11 @@
 				return;
 			}
 
-			//TODO: check if username exists
+			$checkUsernameQuery = mysqli_query($this->con, "SELECT username FROM users WHERE username='$un'");
+			if(mysqli_num_rows($checkUsernameQuery) != 0) {
+				array_push($this->errorArray, Constants::$usernameTaken);
+				return;
+			}
 
 		}
 
@@ -67,7 +99,11 @@
 				return;
 			}
 
-			//TODO: Check that username hasn't already been used
+			$checkEmailQuery = mysqli_query($this->con, "SELECT email FROM users WHERE email='$em'");
+			if(mysqli_num_rows($checkEmailQuery) != 0) {
+				array_push($this->errorArray, Constants::$emailTaken);
+				return;
+			}
 
 		}
 
